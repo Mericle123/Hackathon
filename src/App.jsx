a98ab589670529f8102e4e5cf79acfab60ef503e
NG <ngawangg927@gmail.com>
2024-09-01 21:38:58 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+// import Login from "./component/Login";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      {/* <Routes> */}
+      {/* <Route path="/login" element={<Login />} /> */}
+      {/* </Routes> */}
+   
+
+    </>
+  )
+}
+
+export default App
