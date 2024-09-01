eb78be1b41527c3bfce09431ec3e329316b3af6c
NG <ngawangg927@gmail.com>
2024-09-01 21:44:03 +0600

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from "react";
 import './App.css'
 // import Login from "./component/Login";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       
